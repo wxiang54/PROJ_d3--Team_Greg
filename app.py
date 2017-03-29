dc81857f05ffd89c23809ffbfea54b8d8dbf0a13
jschluger <jschluger@stuy.edu>
2017-03-29 13:03:05 -0400

    starting the flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+import utils
+
+app = Flask(__name__)
+
+#root, two behaviors:
+#    if logged in: redirects you to your feed
+#    if not logged in: displays log in/register page
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
