2d462e4be14ce83efcddeb20826b1629b2712f62
jieyuxu <jieyu.stuy@gmail.com>
2017-04-04 11:03:19 -0400

    label transition

--- static/js/script.js
+++ static/js/script.js
@@ -96,6 +96,17 @@ var stop = function(e) {
 }
 button_animate.addEventListener("click", animate);
 
+function arcTween(d) {
+  var xd = d3.interpolate(x.domain(), [d.x, d.x + d.dx]),
+      yd = d3.interpolate(y.domain(), [d.y, 1]),
+      yr = d3.interpolate(y.range(), [d.y ? 20 : 0, radius]);
+  return function(d, i) {
+    return i
+        ? function(t) { return arc(d); }
+        : function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); return arc(d); };
+  };
+}
+
 function computeTextRotation(d) {
   return (x(d.x + d.dx / 2) - Math.PI / 2) / Math.PI * 180;
 }
@@ -120,6 +131,7 @@ window.onload = function() {
             .attr("dx", "6") // margin
             .attr("dy", ".35em") // vertical-align
             .attr("color", "black")
+            .attr("style", 'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;')
             .text(function(d) { return d.name + "\n" + formatNumber(d.value); });
 	// .text(function(d) { return d.name + "\n" + formatNumber(d.value); });
 
